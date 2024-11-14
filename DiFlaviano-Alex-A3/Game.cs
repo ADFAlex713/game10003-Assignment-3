20658591be490e88198a244d9ea465d96c9cdb82
ADFAlex713 <adiflaviano7@gmail.com>
2024-11-13 23:15:47 -0500

    Add Player and player movement

--- DiFlaviano-Alex-A3/Game.cs
+++ DiFlaviano-Alex-A3/Game.cs
@@ -12,14 +12,23 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
-
+        Player player = new Player();
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
+            Window.SetTitle("2D Game");
+            Window.SetSize(800, 600);
 
+
+            // setup player
+            //player.health = 3;
+            player.moveSpeed = 250;
+            player.color = Color.Gray;
+            player.size = new Vector2(80, 40);
+            player.position = new Vector2(Window.Width - 700, Window.Height / 2);
         }
 
         /// <summary>
@@ -27,7 +36,10 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.OffWhite);
 
+            player.DrawPlayer();
+            player.PlayerMovement();
         }
     }
 }
---
+++ DiFlaviano-Alex-A3/Player.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Numerics;
+
+namespace Game10003
+{
+    public class Player
+    {
+        public Vector2 position;
+        public Vector2 size;
+        public Color color;
+        public float moveSpeed;
+        public int health;
+
+        public void DrawPlayer()
+        {
+            Draw.LineSize = 2;
+            Draw.LineColor = Color.Black;
+            Draw.FillColor = color;
+            Draw.Rectangle(position, size);
+        }
+
+        public void PlayerMovement()
+        {
+            // Move Left
+            if(Input.IsKeyboardKeyDown(KeyboardInput.A))
+            {
+                position.X -= moveSpeed * Time.DeltaTime;
+            }
+            // Move Right
+            if(Input.IsKeyboardKeyDown(KeyboardInput.D))
+            {
+                position.X += moveSpeed * Time.DeltaTime;
+            }
+            // Move Up
+            if(Input.IsKeyboardKeyDown(KeyboardInput.W))
+            {
+                position.Y -= moveSpeed * Time.DeltaTime;
+            }
+            // Move Down
+            if(Input.IsKeyboardKeyDown (KeyboardInput.S))
+            {
+                position.Y += moveSpeed * Time.DeltaTime;
+            }
+
+            // Constrain left
+            if (position.X < 0)
+            {
+                position.X = 0;
+            }
+            // Constrain right
+            if (position.X + size.X > Window.Width / 2)
+            {
+                position.X = (Window.Width/2) - size.X;
+            }
+            // Constrain top
+            if (position.Y < 0)
+            {
+                position.Y = 0;
+            }
+            // Contrain bottom
+            if (position.Y +size.Y > Window.Height)
+            {
+                position.Y= Window.Height - size.Y;
+            }
+        }
+    }
+}
